1e48f15d0f9c44fcff8f009e2ab5e43350816170
Anastasiya Shtrakh <a4evo@yandex.ru>
2018-08-15 10:16:38 +0300

    feat: extracted Book from BookGrid, now search works with  undefined author and img link

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -17,22 +17,45 @@ class BooksGrid extends Component {
 			<ol className="books-grid">
 
 				{books.map( book => (
-					<li key={book.id}>
-						<div className="book">
-							<div className="book-top">
-								<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.thumbnail + '")' }}></div>
-
+						<Book book={book} key={book.id}>
 								<ShelfSelector shelf={book.shelf}
 															shelves={this.props.shelves}/>
-							</div>
-							<div className="book-title">{ book.title }</div>
-							<div className="book-authors">{ (book.authors) ? (book.authors.join(', ')):('Unknown')  }</div>
-						</div>
-					</li>
+					</Book>
 				))}
-		</ol>)
+			</ol>
+		)
 	}
 
+}
+
+class Book extends Component {
+
+	static propTypes = {
+			book: PropTypes.object.isRequired
+		}
+
+	render () {
+		const { book } = this.props
+		const { id, imageLinks, title, authors } = book
+
+		const imgURL = ( imageLinks != undefined && imageLinks.thumbnail != undefined) ? ('url("' + imageLinks.thumbnail + '")') : ('none')
+		const author = (authors) ? (authors.join(', ')):('Unknown')
+
+		return(
+			<li key={id}>
+						<div className="book">
+							<div className="book-top">
+								<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: imgURL }}></div>
+
+								{this.props.children}
+
+							</div>
+							<div className="book-title">{ title }</div>
+							<div className="book-authors">{ author }</div>
+						</div>
+					</li>
+		)
+	}
 }
 
 export default BooksGrid
